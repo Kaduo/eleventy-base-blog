425cfc892a0d6bb46549288aa7a3057b1bdef4c6
Balthazar Potet <balthazarpotet@gmail.com>
2024-12-01 23:29:21 +0100

    bunch of changes

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
+	title: "Arrgh!",
 	url: "https://example.com/",
-	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	language: "fr",
+	description: "argh?",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Balthazar",
+		email: "balthazarpotet@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
---
+++ content/blog/Kiki/kiki.md
@@ -0,0 +1,8 @@
+---
+title: Kiki la petite sorcière
+description: Un super film
+date: "git Last Modified"
+tags: cinéma
+---
+
+Si bien qu'il n'y avait pas qu'un truc...
